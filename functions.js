0fb48c3bc37bd5fbe7413947dac2faa32598cd0d
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-10-22 00:33:23 +1100

    object/array - toEqual

--- functions.js
+++ functions.js
@@ -1,5 +1,12 @@
 const functions = {
-    add: (num1, num2) => num1 + num2 //arrow function
+    add: (num1, num2) => num1 + num2, //arrow function
+    isNull: () => null,
+    checkValue: x => x,
+    createUser: () => {
+        const user = { firstName: 'Barry'}
+        user ['lastName'] = 'Nguyen';
+        return user;
+    } 
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,27 @@
 const functions = require('./functions');
 
+// toBE
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
+});
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+
+//toBeNull
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test('Should be falsy', () => {
+    expect(functions.checkValue(null)).toBeFalsy();
+});
+
+//toEqual - for objects and arrays
+test('User should be Barry Nguyen object', () => {
+    expect(functions.createUser()).toEqual({ firstName: 'Barry', lastName: 'Nguyen'});
 });
